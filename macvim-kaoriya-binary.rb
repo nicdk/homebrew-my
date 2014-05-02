67a434e13fa70774b1bfe5614a0769139a0ee3da
Kei NIKADO <nikado.kei@gmail.com>
2014-05-02 14:49:56 +0900

    homebrew cask formula macvim-kaoriya-binary.

---
+++ macvim-kaoriya-binary.rb
@@ -0,0 +1,8 @@
+class MacvimKaoriyaBinary < Cask
+  url 'https://github.com/splhack/macvim/releases/download/20140501/macvim-kaoriya-20140501.dmg'
+  version '20140501'
+  sha256 'f45deb0f7dc399b248ff857b339542d6a3bdfee5a49bbad28ec09ad9b105bebc'
+
+  homepage 'http://code.google.com/p/macvim-kaoriya/'
+  link 'MacVim.app'
+end
