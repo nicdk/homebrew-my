70702326908fea04ee05e4af62f22e11db01e887
Kei NIKADO <nikado.kei@gmail.com>
2014-05-07 15:48:23 +0900

    (delete comment)

--- menumeters-binary.rb
+++ menumeters-binary.rb
@@ -4,7 +4,6 @@ class MenumetersBinary < Cask
   sha256 'e2c55e42aacb93d0c392058bde3dc35dee5bd7de1824cedacadc2874bfb08927'
 
   homepage 'http://www.ragingmenace.com/software/menumeters/index.html'
-  #head 'http://git.code.sf.net/p/omegat/code'
+
   manual_installer 'MenuMeters Installer.app'
-  #install 'MenuMeters Installer.app'
 end
