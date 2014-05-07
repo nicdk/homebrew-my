0e37cf67e21f7aed2970079d3c5bdec843a6d2a7
Kei NIKADO <nikado.kei@gmail.com>
2014-05-07 15:47:34 +0900

    MenuMeters brew cask Formula.

---
+++ menumeters-binary.rb
@@ -0,0 +1,10 @@
+class MenumetersBinary < Cask
+  url 'http://www.ragingmenace.com/software/download/MenuMeters.dmg'
+  version '1.7'
+  sha256 'e2c55e42aacb93d0c392058bde3dc35dee5bd7de1824cedacadc2874bfb08927'
+
+  homepage 'http://www.ragingmenace.com/software/menumeters/index.html'
+  #head 'http://git.code.sf.net/p/omegat/code'
+  manual_installer 'MenuMeters Installer.app'
+  #install 'MenuMeters Installer.app'
+end
