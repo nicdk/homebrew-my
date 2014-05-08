b5da1d9c7b72d4a0fb55a25963c68c4af4808be5
Kei NIKADO <nikado.kei@gmail.com>
2014-05-08 15:51:08 +0900

    Glance brew cask Formula.

---
+++ glance-binary.rb
@@ -0,0 +1,7 @@
+class GlanceBinary < Cask
+  url 'http://www.glance.net/install/GlanceMac_2.8.zip'
+  version '2.8'
+  sha256 '250528ed7740fe9b2ff3d106ed53b74ae5ba7a9e60f6d33eccec9f010c2012d3'
+  homepage 'http://ww2.glance.net/'
+  install 'GlanceM_2.8.6.68.pkg'
+end
